91a6665afeaf716b7b7c9f678132a01c15fc0454
guochao <463561248@qq.com>
2021-06-15 23:43:35 +0800

    复习

--- array.1.两数之和.js
+++ array.1.两数之和.js
@@ -11,16 +11,30 @@
  * @return {number[]}
  */
 var twoSum = function (nums, target) {
-  const numsObjs = nums.reduce((preValue, num, index) => {
-      preValue[num] = index;
-      return preValue;
+  // 一些容错
+  const numberObjs = nums.reduce((preValue, num, index) => {
+    preValue[num] = index;
+    return preValue
   }, {});
-  for (let i = 0; i < nums.length; i++) {
-    if (numsObjs[target - nums[i]] && numsObjs[target - nums[i]] !== i) {
-      return [i, numsObjs[target - nums[i]]];
+  let i = 0
+  while( i < nums.length){
+    let lastNum = target - nums[i]; 
+    if(numberObjs[lastNum] && numberObjs[lastNum] > i){
+      return [i, numberObjs[lastNum]]
     }
+    i++;
   }
-  return [];
+  return []
+  // const numsObjs = nums.reduce((preValue, num, index) => {
+  //     preValue[num] = index;
+  //     return preValue;
+  // }, {});
+  // for (let i = 0; i < nums.length; i++) {
+  //   if (numsObjs[target - nums[i]] && numsObjs[target - nums[i]] !== i) {
+  //     return [i, numsObjs[target - nums[i]]];
+  //   }
+  // }
+  // return [];
 };
 // console.log(twoSum([1,3,4,2], 6))
 // @lc code=end
--- array.11.盛最多水的容器.js
+++ array.11.盛最多水的容器.js
@@ -11,21 +11,22 @@
  */
 var maxArea = function(height) {
     // 容错处理
-    let left = 0
-    let right = height.length - 1
-    let res = 0
-    while (left < right) {
-        let area = Math.min(height[left], height[right]) * (right - left)
-        if (area > res) res = area
-        if (height[left] > height[right]) {
-            right--
-        } else {
-            left++
+    let left = 0;
+    let right = height.length - 1;
+    let maxArea = 0;
+    while(left < right){
+        let curArea = (right - left) * Math.min(height[left], height[right]);
+        if (curArea > maxArea) maxArea = curArea
+        if(height[left] < height[right]){
+            left ++
+        }else{
+            right --;
         }
     }
-    return res
+    return maxArea
 };
 
-console.log(maxArea([1, 1]))
+// console.log(maxArea([1, 1]))
+// console.log(maxArea([1,8,6,2,5,4,8,3,7]))
 // @lc code=end
 
